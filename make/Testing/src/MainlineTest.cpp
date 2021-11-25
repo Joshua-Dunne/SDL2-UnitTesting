5078bcec1f548494a8d9465af2e1c98ef247f0ac
Joshua-Dunne <c00241588@itcarlow.ie>
2021-11-25 10:20:36 +0000

    Implemented UnitCPP tests to make

--- make/Testing/src/MainlineTest.cpp
+++ make/Testing/src/MainlineTest.cpp
@@ -2,23 +2,67 @@
 #include <assert.h>
 
 #include "../../Production/include/Production.h"
+#include "cppunit/TestCase.h"
+#include "cppunit/TestSuite.h"
+#include "cppunit/TestCaller.h"
+#include "cppunit/TestRunner.h"
+#include "cppunit/TestResult.h"
+#include <cppunit/ui/text/TestRunner.h>
 
 using namespace std;
 
-int main()
-{
-	Production p;
+class LotteryNumberTest : public CppUnit::TestCase { 
+public:
+	Production* testProd;
+	void setUp()
+	{
+		std::cout << "setup";
+		testProd = new Production();
+	}
 
-	p.generateLottery();
-
-	// check for 6 numbers
-	assert(p.genSet.size() == 6);
+	void tearDown() 
+	{
+		std::cout << " teardown";
+		delete testProd;
+	}
 	
-	// check if 6 numbers are within range 1 - 46
-	assert(p.checkRange());
+	void testGeneration()
+	{
+		testProd->generateLottery();
+		CPPUNIT_ASSERT( testProd->genSet.size() == 6 );
+	}
 
-	// check for no duplicates
-	assert(p.checkDuplicates());
+	void testRange()
+	{
+		testProd->generateLottery();
+		CPPUNIT_ASSERT( testProd->checkRange() );
+	}
 
-	cin.get();
+	void testDuplicates()
+	{
+		testProd->generateLottery();
+		CPPUNIT_ASSERT( testProd->checkDuplicates() );
+	}
+
+	static CppUnit::Test *suite()
+  	{
+		CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite( "ComplexNumberTest" );
+		suiteOfTests->addTest( new CppUnit::TestCaller<LotteryNumberTest>(
+							"testGeneration", 
+							&LotteryNumberTest::testGeneration ));
+		suiteOfTests->addTest( new CppUnit::TestCaller<LotteryNumberTest>(
+							"testRange", 
+							&LotteryNumberTest::testRange ));
+		suiteOfTests->addTest( new CppUnit::TestCaller<LotteryNumberTest>(
+							"testDuplicates", 
+							&LotteryNumberTest::testDuplicates ));
+		return suiteOfTests;
+  	}
+};
+
+int main(int argc, char **argv)
+{
+	CppUnit::TextUi::TestRunner runner;
+  	runner.addTest( LotteryNumberTest::suite() );
+  	runner.run();
 }
